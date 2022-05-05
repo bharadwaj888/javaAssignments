a1d413728990cfc79e65c61f98f4d5d23b14de5e
bharadwaj888 <103480607+bharadwaj888@users.noreply.github.com>
2022-05-05 13:25:38 +0530

    assignment1

---
+++ assign1/Main.java
@@ -0,0 +1,34 @@
+package assign1;
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Directory: C:\\Users\\shivv");
+        File directory = new File("C:\\Users\\shivv\\Downloads");
+
+        while(true) {
+            String file_to_search = scan.next();
+            File[] files = directory.listFiles();
+
+            for(File f : files) {
+
+                if (f.getName().contains(file_to_search)) {
+                    System.out.println(f.getAbsolutePath());
+                }
+            }
+
+
+
+            for(File f : files) {
+
+                if (f.getName() == file_to_search) {
+                    System.out.println(f.getAbsolutePath());
+                }
+            }
+        }
+    }
+}
